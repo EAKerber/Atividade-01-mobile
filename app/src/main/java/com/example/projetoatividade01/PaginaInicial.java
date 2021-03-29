2c62b03f00c1ba4cfa52c05e645f8f3ba9c9e395
Eduardo Kerber <Edu.a.kerber@gmail.com>
2021-03-28 21:57:08 -0300

    Adicionado Posts e Albums, funcionando

--- app/src/main/java/com/example/projetoatividade01/PaginaInicial.java
+++ app/src/main/java/com/example/projetoatividade01/PaginaInicial.java
@@ -16,7 +16,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.projetoatividade01.model.Album;
 import com.example.projetoatividade01.model.Comment;
+import com.example.projetoatividade01.model.Post;
 import com.example.projetoatividade01.model.Todo;
 
 import org.json.JSONArray;
@@ -30,8 +32,11 @@ public class PaginaInicial extends AppCompatActivity
         implements Response.Listener<JSONArray>,
         Response.ErrorListener{
 
+    String btnTag = "";
     List<Todo> todos = new ArrayList<>();
     List<Comment> comments = new ArrayList<>();
+    List<Post> posts = new ArrayList<>();
+    List<Album> albums = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,9 +52,13 @@ public class PaginaInicial extends AppCompatActivity
         Button btn2 = (Button)findViewById(R.id.button4);
         btn2.setOnClickListener(this::retornarSplash);
         Button btn3 = (Button)findViewById(R.id.buttonTodo);
-        btn3.setOnClickListener(this::todoOnClick);
+        btn3.setOnClickListener(this::OnClick);
         Button btn4 = (Button)findViewById(R.id.buttonComment);
-        btn4.setOnClickListener(this::commentOnClick);
+        btn4.setOnClickListener(this::OnClick);
+        Button btn5 = (Button)findViewById(R.id.buttonPost);
+        btn5.setOnClickListener(this::OnClick);
+        Button btn6 = (Button)findViewById(R.id.buttonAlbum);
+        btn6.setOnClickListener(this::OnClick);
 
     }
 
@@ -71,41 +80,20 @@ public class PaginaInicial extends AppCompatActivity
         startActivity(intent);
         finish();
     }
-
-    public  void todoOnClick(View view){
-        //Intent intent = getIntent();
-        //Intent intent =
-        LinearLayout ll = findViewById(R.id.principalVerticalSV);
-        ll.removeAllViews();
-        todos.clear();
-        exibeTodo();
-        //startActivity(intent);
-    }
-    public  void commentOnClick(View view){
+    public  void OnClick(View view){
         LinearLayout ll = findViewById(R.id.principalVerticalSV);
         ll.removeAllViews();
         comments.clear();
-        exibeComment();
-    }
+        todos.clear();
+        posts.clear();
+        albums.clear();
 
-    private void exibeComment() {
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        String url = "https://jsonplaceholder.typicode.com/comments";
-        JsonArrayRequest jsonArrayRequest2 = new JsonArrayRequest(Request.Method.GET, url,
-                null,
-                this,
-                this);
-        queue.add(jsonArrayRequest2);
-    }
+        Button btn = (Button)view;
+        btnTag = btn.getTag()+"";
 
-
-    public void exibeTodo(){
-        //Intent intent = new Intent(this, PaginaInicial.class);
-        //startActivity(intent);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://jsonplaceholder.typicode.com/todos";
-
+        String url = "https://jsonplaceholder.typicode.com/"+btn.getTag();
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url,
                 null,
                 this,
@@ -121,71 +109,152 @@ public class PaginaInicial extends AppCompatActivity
 
     @Override
     public void onResponse(JSONArray response) {
-        if(response.length() == 200){
-            try {
-                for (int i = 0; i < response.length(); i++){
-                    JSONObject json = response.getJSONObject(i);
-                    Todo obj = new Todo(json.getInt("userId"),
-                        json.getInt("id"),
-                        json.getString("title"),
-                        json.getBoolean("completed"));
-                    todos.add(obj);
-                }
-                LinearLayout ll = findViewById(R.id.principalVerticalSV);
-                for (Todo obj1 : todos){
-                    Button bt = new Button(this);
-                    bt.setText(obj1.getTitle());
-                    bt.setTag(obj1);
-                    bt.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Button btn = (Button) v;
-                            Todo todo = (Todo)btn.getTag();
-                            Intent intent = new Intent(getApplicationContext(), DetalheTodoActivity.class);
-                            intent.putExtra("objTodo", todo);
-                            Toast.makeText(v.getContext(), todo.getId()+" - "+todo.getTitle(), Toast.LENGTH_LONG).show();
-                            startActivity(intent);
+            switch (btnTag){
+                case "todos":
+
+                    try {
+                    for (int i = 0; i < response.length(); i++){
+                        JSONObject json = response.getJSONObject(i);
+                        Todo obj = new Todo(json.getInt("userId"),
+                                json.getInt("id"),
+                                json.getString("title"),
+                                json.getBoolean("completed"));
+                        todos.add(obj);
+                    }
+                    LinearLayout ll = findViewById(R.id.principalVerticalSV);
+                    for (Todo obj1 : todos){
+                        Button bt = new Button(this);
+                        bt.setText(obj1.getTitle());
+                        bt.setTag(obj1);
+                        bt.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Button btn = (Button) v;
+                                Todo todo = (Todo)btn.getTag();
+                                Intent intent = new Intent(getApplicationContext(), DetalheTodoActivity.class);
+                                intent.putExtra("objTodo", todo);
+                                Toast.makeText(v.getContext(), todo.getId()+" - "+todo.getTitle(), Toast.LENGTH_LONG).show();
+                                startActivity(intent);
+                            }
+                        });
+                        ll.addView(bt);
+                    }
+                } catch (JSONException e) {
+                        Log.e("erro", e.getMessage());
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "comments":
+
+                    try {
+                    for (int i = 0; i < response.length(); i++){
+                        JSONObject json = response.getJSONObject(i);
+                        Comment obj = new Comment(json.getInt("postId"),
+                                json.getInt("id"),
+                                json.getString("name"),
+                                json.getString("email"),
+                                json.getString("body"));
+                        comments.add(obj);
+                    }
+                    LinearLayout ll = findViewById(R.id.principalVerticalSV);
+                    for (Comment obj1 : comments){
+                        Button bt = new Button(this);
+                        bt.setText(obj1.getBody());
+                        bt.setTag(obj1);
+                        bt.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Button btn = (Button) v;
+                                Comment comment = (Comment)btn.getTag();
+                                Intent intent = new Intent(getApplicationContext(), DetalheTodoActivity.class);
+                                intent.putExtra("objTodo", comment);
+                                Toast.makeText(v.getContext(), comment.getId()+" - "+comment.getBody(), Toast.LENGTH_LONG).show();
+                                startActivity(intent);
+                            }
+                        });
+                        ll.addView(bt);
+                    }
+                } catch (JSONException e) {
+                        Log.e("erro", e.getMessage());
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "posts":
+
+                    try {
+                        for (int i = 0; i < response.length(); i++){
+                            JSONObject json = response.getJSONObject(i);
+                            Post obj = new Post(json.getInt("userId"),
+                                    json.getInt("id"),
+                                    json.getString("title"),
+                                    json.getString("body"));
+                            posts.add(obj);
                         }
-                    });
-                    ll.addView(bt);
-                }
-            } catch (JSONException e) {
-                Log.e("erro", e.getMessage());
-                e.printStackTrace();
-            }
-        }else {
-            try {
-                for (int i = 0; i < response.length(); i++){
-                    JSONObject json = response.getJSONObject(i);
-                    Comment obj = new Comment(json.getInt("postId"),
-                            json.getInt("id"),
-                            json.getString("name"),
-                            json.getString("email"),
-                            json.getString("body"));
-                    comments.add(obj);
-                }
-                LinearLayout ll = findViewById(R.id.principalVerticalSV);
-                for (Comment obj1 : comments){
-                    Button bt = new Button(this);
-                    bt.setText(obj1.getBody());
-                    bt.setTag(obj1);
-                    bt.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Button btn = (Button) v;
-                            Comment comment = (Comment)btn.getTag();
-                            Intent intent = new Intent(getApplicationContext(), DetalheTodoActivity.class);
-                            intent.putExtra("objTodo", comment);
-                            Toast.makeText(v.getContext(), comment.getId()+" - "+comment.getBody(), Toast.LENGTH_LONG).show();
-                            startActivity(intent);
+                        LinearLayout ll = findViewById(R.id.principalVerticalSV);
+                        for (Post obj1 : posts){
+                            Button bt = new Button(this);
+                            bt.setText(obj1.getTitle());
+                            bt.setTag(obj1);
+                            bt.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Button btn = (Button) v;
+                                    Post post = (Post) btn.getTag();
+                                    Intent intent = new Intent(getApplicationContext(), DetalheTodoActivity.class);
+                                    intent.putExtra("objTodo", post);
+                                    Toast.makeText(v.getContext(), post.getId()+" - "+post.getTitle(), Toast.LENGTH_LONG).show();
+                                    startActivity(intent);
+                                }
+                            });
+                            ll.addView(bt);
                         }
-                    });
-                    ll.addView(bt);
-                }
-            } catch (JSONException e) {
-                Log.e("erro", e.getMessage());
-                e.printStackTrace();
+                    } catch (JSONException e) {
+                        Log.e("erro", e.getMessage());
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "albums":
+
+                    try {
+                        for (int i = 0; i < response.length(); i++){
+                            JSONObject json = response.getJSONObject(i);
+                            Album obj = new Album(json.getInt("userId"),
+                                    json.getInt("id"),
+                                    json.getString("title"));
+                            albums.add(obj);
+                        }
+                        LinearLayout ll = findViewById(R.id.principalVerticalSV);
+                        for (Album obj1 : albums){
+                            Button bt = new Button(this);
+                            bt.setText(obj1.getTitle());
+                            bt.setTag(obj1);
+                            bt.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Button btn = (Button) v;
+                                    Album album = (Album) btn.getTag();
+                                    Intent intent = new Intent(getApplicationContext(), DetalheTodoActivity.class);
+                                    intent.putExtra("objTodo", album);
+                                    Toast.makeText(v.getContext(), album.getId()+" - "+album.getTitle(), Toast.LENGTH_LONG).show();
+                                    startActivity(intent);
+                                }
+                            });
+                            ll.addView(bt);
+                        }
+                    } catch (JSONException e) {
+                        Log.e("erro", e.getMessage());
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
             }
         }
     }
-}
